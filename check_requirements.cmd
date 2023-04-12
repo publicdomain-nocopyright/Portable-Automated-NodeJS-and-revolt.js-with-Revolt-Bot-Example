70436f297d156b187c2440ba09f26721e3a3d821
Feldwor <BoQsc@users.noreply.github.com>
2023-04-12 11:14:10 +0300

    Update check_requirements.cmd

--- check_requirements.cmd
+++ check_requirements.cmd
@@ -1,16 +1,24 @@
 @ECHO OFF
+SET "requirements_met=true"
+
 IF NOT EXIST %SYSTEMROOT%\System32\curl.exe (
-ECHO curl command not found. Please install curl and add it to PATH environment variable. && PAUSE && EXIT
+ECHO curl command not found. Please install curl and add it to PATH environment variable. && SET "requirements_met=false"
 )
 
 IF NOT EXIST %SYSTEMROOT%\System32\tar.exe (
-ECHO tar command not found. Please install tar and add it to PATH environment variable. && PAUSE && EXIT
+ECHO tar command not found. Please install tar and add it to PATH environment variable. && SET "requirements_met=false"
 )
 
-IF NOT EXIST %SYSTEMROOT%\System32\node.exe (
-ECHO Node.js not found. Please install Node.js and add it to PATH environment variable. && PAUSE && EXIT
-)
+REM IF NOT EXIST %SYSTEMROOT%\System32\node.exe (
+REM ECHO Node.js not found. Please install Node.js and add it to PATH environment variable. && SET "requirements_met=false"
+REM )
+REM 
+REM IF NOT EXIST %SYSTEMROOT%\System32\npm.cmd (
+REM ECHO NPM not found. Please install Node.js NPM and add it to PATH environment variable. && SET "requirements_met=false"
+REM )
 
-IF NOT EXIST %SYSTEMROOT%\System32\npm.cmd (
-ECHO NPM not found. Please install Node.js NPM and add it to PATH environment variable. && PAUSE && EXIT
+IF "%requirements_met%"=="true" (
+ECHO All requirements met. You can now run the script. && PAUSE && EXIT
+) ELSE (
+ECHO Please install the missing requirements and try again. && PAUSE && EXIT
 )
